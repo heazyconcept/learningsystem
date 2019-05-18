d71e63324638b5765d85e8640718a1d5a01d7498
Ezekiel Fadipe <ezekiel.fadipe@outlook.com>
2019-05-18 13:24:05 +0100

    Finished validate Session

--- api/application/controllers/Account.php
+++ api/application/controllers/Account.php
@@ -47,6 +47,7 @@ class Account extends CI_Controller {
     {
         try {
             $this->ConfirmAjaxCall();
+            // log_message("ERROR", serialize($_SERVER));
             $LoginData = (object) $_POST;
             $UserData = $this->users->GetByEmail($LoginData->EmailAddress);
             if (empty((array) $UserData)) {
@@ -62,7 +63,8 @@ class Account extends CI_Controller {
                     "SessionId" => $SessionID,
                     "SessionDate" => $this->utilities->DbTimeFormat(),
                     "IsActive" => 1,
-                    "UserAgent" => $this->utilities->GetUserAgent()
+                    "UserAgent" => $this->utilities->GetUserAgent(),
+                    "IpAddress" => $this->utilities->GetIpAddress()
                 );
                $DbResponse =  $this->customSession->Insert((object) $sessionData);
                if ($DbResponse > 0 ) {
@@ -85,19 +87,42 @@ class Account extends CI_Controller {
         return;
 
     }
-    // public function serverRequest()
-    // {
-    //     $ServerRequest = 'a:54:{s:14:"CONTENT_LENGTH";s:3:"380";s:12:"CONTENT_TYPE";s:68:"multipart/form-data; boundary=----WebKitFormBoundaryF62vYgCvE81jzPXh";s:21:"CONTEXT_DOCUMENT_ROOT";s:25:"/home/bohatty/public_html";s:14:"CONTEXT_PREFIX";s:0:"";s:13:"DOCUMENT_ROOT";s:25:"/home/bohatty/public_html";s:17:"GATEWAY_INTERFACE";s:7:"CGI/1.1";s:5:"HTTPS";s:2:"on";s:11:"HTTP_ACCEPT";s:3:"*/*";s:20:"HTTP_ACCEPT_ENCODING";s:17:"gzip, deflate, br";s:20:"HTTP_ACCEPT_LANGUAGE";s:26:"en-GB,en-US;q=0.9,en;q=0.8";s:15:"HTTP_CONNECTION";s:10:"keep-alive";s:11:"HTTP_COOKIE";s:379:"__tawkuuid=e::bohatty.com::HLdwWjW0rdGvN1vDay0AqZyng9jgHSE47RhJuyOj5qZbC66sjJ9rIZ8CgA9QCVX+::2; timezone=Africa/Lagos; whostmgrsession=root%3a9yeGsGAhbosr6PRf%2ca8042dc20c93e1bde366df45fa4c9cb8; cpsession=bohatty%3aRx2wZTadn4GhuaNd%2cb37658c2cb90dda6b93811ada03254a4; TawkConnectionTime=0; ci_session=3ejm4f9h5p0o1b5fqeonurvnc2nh3aif; csrf_cookie=df3d95aee7932ea59a75d62b5f34313c";s:9:"HTTP_HOST";s:11:"bohatty.com";s:11:"HTTP_ORIGIN";s:19:"https://bohatty.com";s:12:"HTTP_REFERER";s:38:"https://bohatty.com/account/login.aspx";s:15:"HTTP_USER_AGENT";s:114:"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36";s:12:"HTTP_X_HTTPS";s:1:"1";s:21:"HTTP_X_REQUESTED_WITH";s:14:"XMLHttpRequest";s:4:"PATH";s:13:"/bin:/usr/bin";s:16:"PHP_INI_SCAN_DIR";s:67:"/opt/cpanel/ea-php56/root/etc:/opt/cpanel/ea-php56/root/etc/php.d:.";s:12:"QUERY_STRING";s:0:"";s:14:"REDIRECT_HTTPS";s:2:"on";s:19:"REDIRECT_SCRIPT_URI";s:35:"https://bohatty.com/ajax_call/login";s:19:"REDIRECT_SCRIPT_URL";s:16:"/ajax_call/login";s:20:"REDIRECT_SSL_TLS_SNI";s:11:"bohatty.com";s:15:"REDIRECT_STATUS";s:3:"200";s:18:"REDIRECT_UNIQUE_ID";s:27:"XFthejL4o4SNDQ9kNef4VgAAABI";s:12:"REDIRECT_URL";s:16:"/ajax_call/login";s:11:"REMOTE_ADDR";s:13:"154.118.64.51";s:11:"REMOTE_PORT";s:5:"11777";s:14:"REQUEST_METHOD";s:4:"POST";s:14:"REQUEST_SCHEME";s:5:"https";s:11:"REQUEST_URI";s:16:"/ajax_call/login";s:15:"SCRIPT_FILENAME";s:35:"/home/bohatty/public_html/index.php";s:11:"SCRIPT_NAME";s:10:"/index.php";s:10:"SCRIPT_URI";s:35:"https://bohatty.com/ajax_call/login";s:10:"SCRIPT_URL";s:16:"/ajax_call/login";s:11:"SERVER_ADDR";s:13:"199.192.20.57";s:12:"SERVER_ADMIN";s:21:"webmaster@bohatty.com";s:11:"SERVER_NAME";s:11:"bohatty.com";s:11:"SERVER_PORT";s:3:"443";s:15:"SERVER_PROTOCOL";s:8:"HTTP/1.1";s:16:"SERVER_SIGNATURE";s:0:"";s:15:"SERVER_SOFTWARE";s:6:"Apache";s:11:"SSL_TLS_SNI";s:11:"bohatty.com";s:2:"TZ";s:3:"UTC";s:9:"UNIQUE_ID";s:27:"XFthejL4o4SNDQ9kNef4VgAAABI";s:14:"ORIG_PATH_INFO";s:1:"/";s:20:"ORIG_PATH_TRANSLATED";s:35:"/home/bohatty/public_html/index.php";s:8:"PHP_SELF";s:10:"/index.php";s:18:"REQUEST_TIME_FLOAT";d:1549492602.335856914520263671875;s:12:"REQUEST_TIME";i:1549492602;s:4:"argv";a:0:{}s:4:"argc";i:0;}';
+    public function serverRequest()
+    {
+        $ServerRequest = 'a:43:{s:15:"REDIRECT_STATUS";s:3:"200";s:9:"HTTP_HOST";s:9:"localhost";s:15:"HTTP_CONNECTION";s:10:"keep-alive";s:14:"CONTENT_LENGTH";s:2:"51";s:11:"HTTP_ACCEPT";s:3:"*/*";s:11:"HTTP_ORIGIN";s:16:"http://localhost";s:21:"HTTP_X_REQUESTED_WITH";s:14:"XMLHttpRequest";s:15:"HTTP_USER_AGENT";s:115:"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36";s:12:"CONTENT_TYPE";s:48:"application/x-www-form-urlencoded; charset=UTF-8";s:12:"HTTP_REFERER";s:36:"http://localhost/lms/user/login.html";s:20:"HTTP_ACCEPT_ENCODING";s:17:"gzip, deflate, br";s:20:"HTTP_ACCEPT_LANGUAGE";s:26:"en-GB,en-US;q=0.9,en;q=0.8";s:11:"HTTP_COOKIE";s:192:"_ga=GA1.1.1609326466.1556889424; TawkConnectionTime=0; __tawkuuid=e::localhost::93WOH3dfyh3T4jMOmqnZjeOQJlG4B1yswr//Z9yG4YroTwHgT5gFUoqY+KK5zvqg::2; ci_session=20ohdq800alev63ru68upaaa3nhpnlks";s:4:"PATH";s:402:"C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\Program Files\Git\cmd;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\dotnet\;C:\Program Files\Microsoft SQL Server\130\Tools\Binn\;C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\170\Tools\Binn\;C:\Users\uridium-Ezekiel\AppData\Local\Microsoft\WindowsApps;C:\Users\uridium-Ezekiel\.dotnet\tools";s:10:"SystemRoot";s:10:"C:\WINDOWS";s:7:"COMSPEC";s:27:"C:\WINDOWS\system32\cmd.exe";s:7:"PATHEXT";s:53:".COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;.MSC";s:6:"WINDIR";s:10:"C:\WINDOWS";s:16:"SERVER_SIGNATURE";s:80:"<address>Apache/2.4.37 (Win64) PHP/7.2.14 Server at localhost Port 80</address>
+            ";s:15:"SERVER_SOFTWARE";s:32:"Apache/2.4.37 (Win64) PHP/7.2.14";s:11:"SERVER_NAME";s:9:"localhost";s:11:"SERVER_ADDR";s:3:"::1";s:11:"SERVER_PORT";s:2:"80";s:11:"REMOTE_ADDR";s:3:"::1";s:13:"DOCUMENT_ROOT";s:13:"C:/wamp64/www";s:14:"REQUEST_SCHEME";s:4:"http";s:14:"CONTEXT_PREFIX";s:0:"";s:21:"CONTEXT_DOCUMENT_ROOT";s:13:"C:/wamp64/www";s:12:"SERVER_ADMIN";s:29:"wampserver@wampserver.invalid";s:15:"SCRIPT_FILENAME";s:31:"C:/wamp64/www/lms/api/index.php";s:11:"REMOTE_PORT";s:5:"54545";s:12:"REDIRECT_URL";s:22:"/lms/api/Account/Login";s:17:"GATEWAY_INTERFACE";s:7:"CGI/1.1";s:15:"SERVER_PROTOCOL";s:8:"HTTP/1.1";s:14:"REQUEST_METHOD";s:4:"POST";s:12:"QUERY_STRING";s:0:"";s:11:"REQUEST_URI";s:22:"/lms/api/Account/Login";s:11:"SCRIPT_NAME";s:18:"/lms/api/index.php";s:9:"PATH_INFO";s:14:"/Account/Login";s:15:"PATH_TRANSLATED";s:27:"C:\wamp64\www\Account\Login";s:8:"PHP_SELF";s:32:"/lms/api/index.php/Account/Login";s:18:"REQUEST_TIME_FLOAT";d:1558130657.367;s:12:"REQUEST_TIME";i:1558130657;}';
 
-    //     $serverArray = unserialize($ServerRequest);
+        $serverArray = unserialize($ServerRequest);
 
         
-    //     echo "<pre>";
-    //     print_r ((object) $serverArray );
-    //     echo "</pre>";
+        echo "<pre>";
+        print_r ((object) $serverArray );
+        echo "</pre>";
         
 
-    // }
+    }
+    public function ValidateSession( string $sessionId)
+    {
+      try {
+         $sessionData = $this->customSession->Get($sessionId);
+         if (!empty($sessionData)) {
+            $IpAddress = $this->utilities->GetIpAddress();
+            $userAgent = $this->utilities->GetUserAgent();
+            if ($sessionData->IsActive == 1 && $IpAddress == $sessionData->IpAddress && $userAgent == $sessionData->UserAgent) {
+               echo $this->utilities->outputMessage("success");
+               return;
+            }
+             
+         }
+       
+
+      } catch (\Throwable $th) {
+         $this->utilities->LogError($th);
+        
+      }
+      echo $this->utilities->outputMessage("error", "Internal server error");
+      return;
+    }
     private function ConfirmAjaxCall()
     {
         if (empty($_REQUEST)) {
--- api/application/libraries/Utilities.php
+++ api/application/libraries/Utilities.php
@@ -220,19 +220,19 @@ class Utilities
     {
       return  $_SERVER['HTTP_USER_AGENT'];
     }
-    // public function getRealIpAddr()
-    // {
-    //     if (!empty($_SERVER['HTTP_CLIENT_IP'])) //check ip from share internet
-    //     {
-    //         $ip = $_SERVER['HTTP_CLIENT_IP'];
-    //     } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) //to check ip is pass from proxy
-    //     {
-    //         $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    //     } else {
-    //         $ip = $_SERVER['REMOTE_ADDR'];
-    //     }
-    //     return $ip;
-    // }
+    public function GetIpAddress():string
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) //check ip from share internet
+        {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) //to check ip is pass from proxy
+        {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
 
     
 
